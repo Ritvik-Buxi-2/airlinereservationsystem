b822b3c706a088181ecd10b95a76efeef52224f9
Muhammad Hussaini <hussainimuhammad999@gmail.com>
2023-06-02 14:01:18 +0530

    Added database connection

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ const hbs = require("hbs");
 const path = require("path");
 
 const mainRouter = require("./router/mainRouter");
+const db = require("./modules/db");
 
 const app = express();
 
 app.set("view engine", "hbs");
 hbs.registerPartials(path.join(__dirname, "/views/partials"), function (err) {
   if (err) console.error(err);
+});
+
+db.connect((e) => {
+  if (e) console.error(e);
+  else console.log("Database connected");
 });
 
 app.use("/", mainRouter);
